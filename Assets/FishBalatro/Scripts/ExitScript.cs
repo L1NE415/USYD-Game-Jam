6c32af6ae633883643b989fb336710a8e33d8aee
TomSDropBear <thshelswell@gmail.com>
2026-06-23 16:33:33 +1000

    Added Quit Game Function

---
+++ Assets/FishBalatro/Scripts/ExitScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            #if UNITY_EDITOR
+            // This stops play mode if you are testing inside the Unity Editor
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            // This closes the actual built application (.exe, .app, etc.)
+                Application.Quit();
+            #endif
+        }
+    }
+}
